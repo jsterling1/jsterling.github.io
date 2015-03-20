cad8a1c5185546219afaec2f7269842df9112725
jsterling1 <jsterling@kippnyccp.org>
2015-03-20 13:31:29 -0400

    Update javascript.js

--- shoppinglist/javascript.js
+++ shoppinglist/javascript.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-  $('#add').on('click',function(){
-  var item = $('#placeholder').val();
-  $('ul').append('<li>'+item+'</li>');
-  });
-  $('#clear').on('click',function(){
-  $('ul').empty();
-  });
-  $('#add').keypress(function(){
-    if(keycode == '13'){
-   $('ul').append('<li>'+item+'</li>');
-    }
-});
+   $('button').on('click',function(){
+    var input=$('input').val();
+    $('#mydiv').append("<p>"+input+"</p>");
+   });
 });
