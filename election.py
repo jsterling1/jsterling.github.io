9a7b39a77926300f022efa03d91a20bcb1439739
jsterling1 <jsterling@kippnyccp.org>
2015-05-12 13:37:21 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,15 +1,15 @@
 from random import randint
 wins = 0
 
-for i in range(10000):
+for i in range(10000): #repeat the process a thousand times
   regions=0
   if randint(1,100) <= 87:
-    regions = regions + 1
+    regions = regions + 1 #add to the amount of wins
    if randint(1,100) <= 65:
-    regions = regions + 1
+    regions = regions + 1 #add to the amount of wins
    if randint(1,100) <= 17:
-    regions = regions + 1
+    regions = regions + 1 #add to the amount of wins
   if regions >= 2:
-    print "You win the election!"
-  else:
-    print "You did not win the election..."
+    wins = wins + 1
+
+
