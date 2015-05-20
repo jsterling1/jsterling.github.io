5a088a4aa1e2850eae8f4207b27ac4616d0c88ce
jsterling1 <jsterling@kippnyccp.org>
2015-05-20 13:09:21 -0400

    Update Basic_Maze.py

--- Basic_Maze.py
+++ Basic_Maze.py
@@ -1,8 +1,20 @@
-from gopigo import
+from gopigo import *
 import time
 import math
+
 set_speed(100)
+
 enc_tgt(1,1,107)
-time.sleep(4)
+time.sleep(.1)
 fwd()
-time.sleep(2)
+time.sleep(3)
+
+def turn_right():
+  enc_tgt(1,0,18)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+  
+enc_tgt(1,1,54)
+  time.sleep()
+  
